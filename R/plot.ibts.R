350857fe69e11e2d79614318d0ad52a898b562ce
Christoph Häni <christoph.haeni@bfh.ch>
2021-10-27 16:34:29 +0200

    Fix NA issue

--- R/plot.ibts.R
+++ R/plot.ibts.R
@@ -51,8 +51,8 @@ plot.ibts <- function(x, column = seq.int(min(2,ncol(x))), se = NULL, xlim = NUL
 		X <- as.data.frame(merge(x,column),keepAtts=FALSE)
         if (colClasses(x) == 'circ' && colClasses(column) == 'circ') {
             dif <- X[, 2] - X[, 1]
-            dif[dif < -180] <- dif[dif < -180] + 360
-            dif[dif > 180] <- dif[dif > 180] - 360
+            dif[which(dif < -180)] <- dif[which(dif < -180)] + 360
+            dif[which(dif > 180)] <- dif[which(dif > 180)] - 360
             X[, 2] <- X[, 1] + dif
         }
 		if(missing(lty)) lty <- NULL
