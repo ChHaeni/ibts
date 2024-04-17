9bf84240f0ad59699fb4d2d62b4b36c2d4b8dc5b
Christoph Häni <christoph.haeni@bfh.ch>
2024-04-17 14:09:40 +0200

    Allow vector input in parse_time_diff()

--- R/parse_time_diff.R
+++ R/parse_time_diff.R
@@ -1,32 +1,40 @@
 parse_time_diff <-
 function(x){
-	if(!is.numeric(x)){
-		if(is.difftime(x)){
-			y <- as.numeric(x,"secs")
-		} else {
-			if(sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)(.*)","\\4",x) != ""){
-				y <- parse_time_diff(sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)(.*)","\\4",x))
-				x <- sub("^([-]*[0-9]*[.]?[0-9]*[ ]*[a-z]*)(.*)","\\1",x)
-			} else {
-				y <- 0
-			}
+    if (length(x) == 1) {
+        if(!is.numeric(x)){
+            if(is.difftime(x)){
+                y <- as.numeric(x,"secs")
+            } else {
+                if(sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)(.*)","\\4",x) != ""){
+                    y <- parse_time_diff(sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)(.*)","\\4",x))
+                    x <- sub("^([-]*[0-9]*[.]?[0-9]*[ ]*[a-z]*)(.*)","\\1",x)
+                } else {
+                    y <- 0
+                }
 
-			pm <- sub("^([-]*)[ ]*([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\1",x)
-			num <- sub("^([-]*)[ ]*([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\2",x)
-			unt <- sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\3",x)
-			if(num!=""){
-				num <- as.numeric(num)
-			} else {
-				num <- 0
-			}
-			if(unt=="")unt <- "secs"
-			y <- y + as.numeric(as.difftime(num,units=unt),"secs")
-			if(pm!=""){
-				y <- -y
-			}
-		}
-		return(y)	
-	} else {
-		return(x)
-	}
+                pm <- sub("^([-]*)[ ]*([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\1",x)
+                num <- sub("^([-]*)[ ]*([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\2",x)
+                unt <- sub("^([-]*)([0-9]*[.]?[0-9]*)[ ]*([a-z]*)","\\3",x)
+                if(num!=""){
+                    num <- as.numeric(num)
+                } else {
+                    num <- 0
+                }
+                if(unt=="")unt <- "secs"
+                y <- y + as.numeric(as.difftime(num,units=unt),"secs")
+                if(pm!=""){
+                    y <- -y
+                }
+            }
+            return(y)	
+        } else {
+            return(x)
+        }
+    }
+    # length > 1
+    out <- numeric(length(x))
+    for (i in seq_along(x)) {
+        out[i] <- parse_time_diff(x[i])
+    }
+    out
 }
