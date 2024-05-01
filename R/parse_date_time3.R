5ef17dee27904dd54d22066b5529786d93338621
Christoph Häni <christoph.haeni@bfh.ch>
2024-05-01 16:56:32 +0200

    Fix parsing error on leading/trailing whitespace

--- R/parse_date_time3.R
+++ R/parse_date_time3.R
@@ -1,4 +1,6 @@
 parse_date_time3 <- function(x, orders = getOption("time.orders"), tz = tzone(x), quiet = TRUE) {
+    # clean whitespace in x argument
+    x <- trimws(x)
 	out <- parse_date_time(x, orders = orders, tz = tz, quiet = quiet, exact = TRUE)
     if (anyNA(out)) {
         out[is.na(out)] <- parse_date_time(x[is.na(out)], orders = orders, tz = tz, quiet = quiet)
