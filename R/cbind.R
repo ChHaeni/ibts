f400ae619f1edfa7bd078e90fc973d44bad5ab45
Christoph Häni <christoph.haeni@bfh.ch>
2024-07-23 23:42:55 +0200

    Remove cbind/rbind "generics"

--- R/cbind.R
+++
@@ -1,9 +0,0 @@
-cbind <-
-function (..., deparse.level = 1){
-	if(any(sapply(list(...),inherits,"ibts"))){
-		return(cbind.ibts(...))
-	} else {
-		# return(.Internal(cbind(deparse.level, ...)))
-		return(base::cbind(...,deparse.level=deparse.level))
-	}
-}
--- R/rbind.R
+++
@@ -1,9 +0,0 @@
-rbind <-
-function (..., deparse.level = 1){
-	if(all(sapply(list(...),inherits,"ibts"))){
-		return(rbind.ibts(...))
-	} else {
-		# return(.Internal(rbind(deparse.level, ...)))
-		return(base::rbind(...,deparse.level=deparse.level))
-	}
-}
