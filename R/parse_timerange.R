a2be0bd7a0034bbf305c39bec991f54c472780b3
Christoph Häni <christoph.haeni@bfh.ch>
2025-10-13 16:11:16 +0200

    Fix %+%/%-% always providing time range

--- R/parse_timerange.R
+++ R/parse_timerange.R
@@ -1,12 +1,18 @@
 parse_timerange <- function(x, tz = "") {
 	seps <- getOption("time.separators")
 	split_char <- seps[sapply(seps, grepl, x)]
-	if (length(split_char)) {
+	if (lsc <- length(split_char)) {
 		x <- unlist(strsplit(x, split_char, fixed = TRUE))
 	}
 	out <- parse_date_time3(x, tz = tz)
-    if (is.na(out[2])) {
-        out[2] <- parse_date_time3(paste(date(out[1]), x[2]), tz = tz)
+    if (lsc && is.na(out[2])) {
+        if (length(x) == 2 && grepl('\\S', x[2])) {
+            out[2] <- parse_date_time3(paste(date(out[1]), x[2]), tz = tz)
+        } else {
+            out[2] <- Inf
+        }
+    } else if (lsc && is.na(out[1])) {
+        out[1] <- -Inf
     }
     out
 }
