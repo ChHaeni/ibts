af58b37f21071adb332b5e4d074628ac5f30e936
Christoph Häni <christoph.haeni@bfh.ch>
2026-01-13 08:30:15 +0100

    Fix c.POSIXct and add lt method

--- R/c.R
+++ R/c.R
@@ -1,5 +1,20 @@
 
-c.POSIXct <- function (..., recursive = FALSE){
-    tz <- tzone(..1)
-    .POSIXct(c(unlist(lapply(list(...),function(x,tz)unclass(with_tz(x,tz)),tz=tz))),tz=tz)
+c.POSIXct <- function (..., recursive = FALSE, tz = tzone(..1)) {
+    # convert to numeric vector
+    out <- unlist(lapply(list(...), function(x) as.numeric(with_tz(x, tzone = tz))))
+    # convert to POSIXct
+    .POSIXct(out, tz = tz)
+}
+
+c.POSIXlt <- function (..., recursive = FALSE, tz = tzone(..1), lt = all_lt) {
+    # check if all lt
+    all_lt <- all(sapply(list(...), \(x) inherits(x, 'POSIXlt')))
+    # convert to numeric vector
+    out <- unlist(lapply(list(...), function(x) as.numeric(with_tz(x, tzone = tz))))
+    # convert to POSIX
+    if (lt) {
+        as.POSIXlt(out, tz = tz)
+    } else {
+        .POSIXct(out, tz = tz)
+    }
 }
