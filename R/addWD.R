517c8ea4bbfbf212b38acc109a031b05fff90ed0
Christoph Häni <christoph.haeni@bfh.ch>
2026-05-27 15:38:37 +0200

    Make addWD simpler

--- R/addWD.R
+++ R/addWD.R
@@ -1,18 +1,65 @@
-addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30
-  , U = NULL, addUlegend = FALSE, ULegendPos = "right", inset = c(0.04, 0)
-  , breaks = function (x) median(x, na.rm = TRUE) * c(0.5, 1, 2), ...){
+addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30,
+  WD_col = c('wd', 'wdir'), U_col = c('ws', 'u'), addUlegend = FALSE, 
+  ULegendPos = "right", inset = c(0.04, 0), pool.args = list(),
+  breaks = function (x) median(x, na.rm = TRUE) * c(0.5, 1, 2), ...){
 
   usr <- par("usr")
   pin <- par("pin")
   dx <- diff(usr[1:2])
   dy <- diff(usr[3:4])
 
-  if (!is.null(U[[1]])){
-    ddy <- dy / frac * U[[1]]
+  # check column names (WD)
+  col_nms <- names(WD)
+  if (is.numeric(WD_col)) {
+      wd_col <- WD_col
+  } else if (missing(WD_col)) {
+      wd_col <- which(tolower(col_nms) %in% WD_col)
   } else {
-    ddy <- dy / frac
+      wd_col <- which(col_nms %in% WD_col)
   }
+  if (length(wd_col) == 0 && ncol(WD) == 1) {
+      wd_col <- 1
+  }
+  if (length(wd_col) > 1) {
+    stop('WD column not found! Please specify a valid column through argument "WD_col"')
+  }
+  # pool WD?
+  if (length(pool.args) > 0) {
+      WD <- do.call(pool, c(list(dat = WD), pool.args))
+  }
+  # check column names (U)
+  U <- NULL
+  if (is.ibts(U_col)) {
+      U <- U_col
+      # pool U?
+      if (length(pool.args) > 0) {
+          U <- do.call(pool, c(list(dat = U), pool.args))
+      }
+  } else if (ncol(WD) > 1) {
+      if (is.numeric(U_col)) {
+          u_col <- U_col
+      } else if (missing(U_col)) {
+          u_col <- which(tolower(col_nms) %in% U_col)
+      } else {
+          u_col <- which(col_nms %in% U_col)
+      }
+      if (length(u_col) > 1) {
+        stop('U column not unique! Please specify a valid column through argument "U_col"')
+      } else if (length(u_col) == 1) {
+          U <- WD[, u_col]
+      } else if (ncol(WD) == 2) {
+          U <- WD[, -wd_col]
+      }
+  }
+  # fix WD
+  WD <- WD[, wd_col]
 
+  # scale dy by U
+  if (is.null(U)) {
+    ddy <- dy / frac
+  } else {
+    ddy <- dy / frac * U[[1]]
+  }
   ddx <- ddy / dy * dx / pin[1] * pin[2]
 
   if (is.null(y0)){
@@ -26,7 +73,6 @@ addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30
   }
 
   for(i in seq_along(WD[[1]])){
-
     wd1 <- WD[i]
     if(!is.na(wd1[[1]])){
       mx <- mt(wd1)
@@ -36,7 +82,6 @@ addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30
 
       vnx <- -sin(rad1)*ddx[i]
       vny <- cos(rad1)*ddy[i]
-      # browser()
       arrows(mx - vnx/2,my - vny/2,mx + vnx/2,my + vny/2,lwd=lwd,length=if(missing(length)) 0.35*ddy[i]*pin[2]/dy else length,angle=angle,...)
     }
   }
@@ -54,11 +99,9 @@ addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30
     lens <- dx/frac*Us/pin[1]*pin[2]
     # lens <- lens/dy*dx
     lns <- if(missing(length)) 0.35*lens else rep(length,3)
-    # browser()
         txt <- sprintf("%1.1f m/s",Us)
         text.width <- max(abs(strwidth(txt, units = "user")))
     text.height <- max(abs(strheight(txt, units = "user")))*0.55
-    # browser()
     dly0 <- pmax(dy/dx*lns*sin(angle/180*pi)/pin[2]*pin[1],text.height) + 0.005*dy
     dly <- cumsum(c(0,dly0)) + cumsum(c(0,0,dly0[-length(dly0)]))
     inset <- rep_len(inset, 2)
@@ -74,12 +117,10 @@ addWD <- function (WD, frac = 25, y0 = NULL, lwd = 3, length = NULL, angle = 30
             h + insety, topleft = , top = , topright = usr[4L] - 
             insety, left = , right = , center = (usr[3L] + 
             usr[4L] + h)/2)
-        # browser()
         # draw
         for(i in seq_along(Us)){
           x0 <- left + text.width
           y0 <- top - dly[i + 1]
-          # browser()
           arrows(x0,y0,x0 + lens[i],y0,lwd=lwd,length=lns[i]*pin[1]/dx,angle=angle,...)
           text(left,y0,txt[i])
         }
